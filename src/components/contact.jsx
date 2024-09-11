6e00f2c6caf4f0ffa7bd60262f4aa19639ef7412
leonalsaksham1 <sakshamshah1234@gmail.com>
2024-09-11 16:41:35 +0545

    comment here

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -7,22 +7,21 @@ const initialState = {
   email: "",
   message: "",
 };
+
 export const Contact = (props) => {
   const [{ name, email, message }, setState] = useState(initialState);
   const [successMessage, setSuccessMessage] = useState("");
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setState((prevState) => ({ ...prevState, [name]: value }));
   };
+
   const clearState = () => setState({ ...initialState });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(name, email, message);
     clearState();
-    {
-      /* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */
-    }
 
     emailjs
       .sendForm(
@@ -33,9 +32,8 @@ export const Contact = (props) => {
       )
       .then(
         (result) => {
-          console.log(result.text);
           setSuccessMessage("Your message has been sent successfully.");
-          clearState(); // Clear the form after successful submission
+          clearState();
         },
         (error) => {
           console.log(error.text);
@@ -47,138 +45,145 @@ export const Contact = (props) => {
     <div>
       <div id="contact">
         <div className="container">
-          <div className="col-md-5  contact-info">
-            <div className="section-title">
-              <h2>Contact Info</h2>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-map-marker"></i> Address
-                </span>
-                {props.data ? props.data.address : "loading"}
-              </p>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-phone"></i> Phone
-                </span>{" "}
-                {props.data ? props.data.phone : "loading"}
-              </p>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-envelope-o"></i> Email
-                </span>{" "}
-                {props.data ? props.data.email : "loading"}
-              </p>
-            </div>
-            <div className="row">
-              <div className="social">
-                <ul>
-                  <li>
-                    <a href={props.data ? props.data.facebook : "/"}>
-                      <i className="fa fa-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.twitter : "/"}>
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
-                    </a>
-                  </li>
-                </ul>
+          <div className="row">
+            {/* Left Column: Contact Info and Get In Touch Form */}
+            <div className="col-md-6">
+              {/* Contact Info Section */}
+              <div className="contact-info">
+                <div className="section-title">
+                  <h2>Contact Info</h2>
+                </div>
+                <div className="contact-item">
+                  <p>
+                    <span>
+                      <i className="fa fa-map-marker"></i> Address
+                    </span>
+                    {props.data ? props.data.address : "loading"}
+                  </p>
+                </div>
+                <div className="contact-item">
+                  <p>
+                    <span>
+                      <i className="fa fa-phone"></i> Phone
+                    </span>
+                    {props.data ? props.data.phone : "loading"}
+                  </p>
+                </div>
+                <div className="contact-item">
+                  <p>
+                    <span>
+                      <i className="fa fa-envelope-o"></i> Email
+                    </span>
+                    {props.data ? props.data.email : "loading"}
+                  </p>
+                </div>
+                <div className="social">
+                  <ul>
+                    <li>
+                      <a href={props.data ? props.data.facebook : "/"}>
+                        <i className="fa fa-facebook"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href={props.data ? props.data.twitter : "/"}>
+                        <i className="fa fa-twitter"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href={props.data ? props.data.youtube : "/"}>
+                        <i className="fa fa-youtube"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="col-md-6 col-md-offset-1">
-            <div className="row">
-              <div className="section-title">
-                <h2>Get In Touch</h2>
-                <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
-                </p>
-              </div>
-              <form name="sentMessage" validate onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="form-control"
-                        placeholder="Name"
-                        required
-                        onChange={handleChange}
-                        value={name}
-                      />
-                      <p className="help-block text-danger"></p>
+
+              {/* Get In Touch Form */}
+              <div className="get-in-touch">
+                <div className="section-title">
+                  <h2>Get In Touch</h2>
+                  <p>
+                    Please fill out the form below to send us an email and we
+                    will get back to you as soon as possible.
+                  </p>
+                </div>
+                <form name="sentMessage" validate onSubmit={handleSubmit}>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          id="name"
+                          name="name"
+                          className="form-control"
+                          placeholder="Name"
+                          required
+                          onChange={handleChange}
+                          value={name}
+                        />
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <input
+                          type="email"
+                          id="email"
+                          name="email"
+                          className="form-control"
+                          placeholder="Email"
+                          required
+                          onChange={handleChange}
+                          value={email}
+                        />
+                      </div>
                     </div>
                   </div>
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="form-control"
-                        placeholder="Email"
-                        required
-                        onChange={handleChange}
-                        value={email}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
+                  <div className="form-group">
+                    <textarea
+                      name="message"
+                      id="message"
+                      className="form-control"
+                      rows="4"
+                      placeholder="Message"
+                      required
+                      onChange={handleChange}
+                      value={message}
+                    ></textarea>
                   </div>
-                </div>
-                <div className="form-group">
-                  <textarea
-                    name="message"
-                    id="message"
-                    className="form-control"
-                    rows="4"
-                    placeholder="Message"
-                    required
-                    onChange={handleChange}
-                    value={message}
-                  ></textarea>
-                  <p className="help-block text-danger"></p>
-                </div>
-                <div id="success">
-                  <p>{successMessage}</p>
-                </div>
-                <button
-                  disabled={successMessage}
-                  type="submit"
-                  className="btn btn-custom btn-lg"
-                >
-                  Send Message
-                </button>
-              </form>
+                  <div id="success">
+                    <p>{successMessage}</p>
+                  </div>
+                  <button
+                    disabled={successMessage}
+                    type="submit"
+                    className="btn btn-custom btn-lg"
+                  >
+                    Send Message
+                  </button>
+                </form>
+              </div>
+            </div>
+
+            {/* Right Column: Google Map */}
+            <div className="col-md-6">
+              <div style={{ width: "100%", height: "450px", border: "none" }}>
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14130.840975075269!2d85.32620695884457!3d27.69534911902795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19a77520a339%3A0x4df14616bdef4f1c!2sSoftech%20Foundation%20Pvt.Ltd.!5e0!3m2!1sen!2snp!4v1726047867016!5m2!1sen!2snp"
+                  width="100%"
+                  height="450"
+                  style={{ border: "none" }}
+                  allowFullScreen
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                  title="Google Maps"
+                ></iframe>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div style={{ width: "100%", height: "450px", border: "none" }}>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14130.840975075269!2d85.32620695884457!3d27.69534911902795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19a77520a339%3A0x4df14616bdef4f1c!2sSoftech%20Foundation%20Pvt.Ltd.!5e0!3m2!1sen!2snp!4v1726047867016!5m2!1sen!2snp"
-        width="1600"
-        height="450"
-        style={{ border: "none" }}
-        allowFullScreen
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-        title="Google Maps"
-      ></iframe>
-    </div>
+
+      {/* Footer */}
       <div id="footer">
         <div className="container text-center">
           <p>&copy; 2024 Saksham Bikram Shah. All Rights Reserved.</p>
