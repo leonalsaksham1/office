fec07a543decaaef7c35c9fac4270a2f6664389f
Sandeep Dangol <sandeep.dangol@ekbana.net>
2024-09-11 13:33:45 +0545

    home

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -55,9 +55,32 @@ export const Navigation = (props) => {
           id="bs-example-navbar-collapse-1"
         >
           <ul className="nav navbar-nav navbar-right">
-          <li>
-              <a href="#home" className="page-scroll">
-                Home
+            <li>
+              <a href="#page-top" className="page-scroll">
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 512 512"
+                  height="1.3em"
+                  width="1.3em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="32"
+                    d="M80 212v236a16 16 0 0 0 16 16h96V328a24 24 0 0 1 24-24h80a24 24 0 0 1 24 24v136h96a16 16 0 0 0 16-16V212"
+                  ></path>
+                  <path
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="32"
+                    d="M480 256 266.89 52c-5-5.28-16.69-5.34-21.78 0L32 256m368-77V64h-48v69"
+                  ></path>
+                </svg>
               </a>
             </li>
             <li>
